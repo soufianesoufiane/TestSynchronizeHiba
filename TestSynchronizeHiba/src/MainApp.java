d3519ff6c292ee36114efb8b86028a1fc1583a15
soufianesoufiane <laanibi.soufiane@gmail.com>
2014-02-19 15:02:57 +0000

    ;hvfj

---
+++ TestSynchronizeHiba/src/MainApp.java
@@ -0,0 +1,15 @@
+public class MainApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello From Soufiane & Hiba !!!");
+		System.out.println("Are u here ?");
+		System.out.println("The third Change");
+		System.out.println("The Fouth Change");
+		System.out.println("The fifth change");
+		System.out.println("The sixth change");
+	}
+}
